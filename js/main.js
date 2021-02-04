3465606573858d9deb7e5368deb8a15edaad6d7b
Valeri <korsso74@gmail.com>
2021-02-04 14:15:03 +0200

    commit-10

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(function(){
-
+   
+    $('.menu__btn').on('click', function() {
+      $('menu__list').toggleClass('menu__list--active')
+    });
    
 
     $('.heroes__slider-img').slick({
@@ -8,6 +11,7 @@ $(function(){
      asNavFor: '.heroes__slider-text',
      prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"</button>',
      nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"</button>'
+     
     });
 
   
